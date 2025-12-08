3849e4299ce7f3c3b9ace919cddf6275c396940d
Bharath <brraju7358@gmail.com>
2025-12-08 20:51:03 +0530

    ifelse and elif learned

---
+++ elif.py
@@ -0,0 +1,21 @@
+mark=int(input("Marks Acquired in 12th grade:"))
+#name=input()
+#add=input()
+#ph=int(input())
+
+if(mark>70):
+   
+    
+    print("Your Eligible for Admission.Please Provide Your Details asked below:")
+    name=input("Name:")
+    add=input("Location:")
+    ph=int(input("contact:"))
+    print("")
+    print("Verified:")
+    print("Name:",name)
+    print("Address:",add)
+    print("phone Numer:",ph)
+    print("Thanks for Uploading")
+
+else:
+    print("Better Luck Next Time.")
--- ifelse.py
+++ ifelse.py
@@ -1,9 +1,39 @@
-mark=int(input("Enter mark:"))
-avg=mark/2
+#mark=int(input("Enter mark:"))
+#avg=mark/2
 
-if(mark/2>=70):
-    print("avg is ",avg,"passed")
+#if(mark/2>=70):
+ #   print("avg is ",avg,"passed")
+#else:
+ #   print("Average",avg,"is low Retry")
+
+
+#a=int(input("Enter a value:"))
+#if(a%5==0 and a%3==0):
+ #   print("Divide by 3 and 5")
+#else:
+ #   print("Not divided by 3 and 5")
+#if(a<=35):
+    #print("poor student")
+#elif(a>35 and a<=70):
+ #   print("average student")
+#else:
+ #   print("good student")
+
+print("Simple Calculator")
+a=float(input("Enter a value:"))
+c=input()
+b=float(input("Enter b value:"))
+
+
+if(c=="+"):
+    print("Addition: ",a+b)
+elif(c=="-"):
+    print("subtracted value:",a-b)
+elif(c=="*"):
+    print("Multiplied value:",a*b)
+elif(c=="/"):
+    print("Division:",a/b)
+elif(c=="%"):
+    print("Reminder after division:",a%b)
 else:
-    print("Average",avg,"is low Retry")
-
-
+    print("invalid operation")
